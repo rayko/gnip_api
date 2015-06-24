3921b6fdcfe8f06fc200bb1498333bc26ccb55a7
Rayko <rayko.drg@gmail.com>
2015-06-24 18:17:01 -0300

    Added specs for some of the methods of PowerTrack::Stream

--- spec/gnip_api/apis/power_track/stream_spec.rb
+++ spec/gnip_api/apis/power_track/stream_spec.rb
@@ -1,5 +1,50 @@
 require 'spec_helper'
 
 describe GnipApi::Apis::PowerTrack::Stream do
-  pending
+  before do
+    configure_gem
+    @stream = GnipApi::Apis::PowerTrack::Stream.new(:source => 'source', :stream => 'stream')
+  end
+
+  describe '#handle_message' do
+    before do
+      @json = File.read('spec/fixtures/activities/real_activity.json')
+    end
+    
+    it 'bulds a Message object from the json' do
+      message = @stream.handle_message(@json)
+      expect(message.class).to eq(Gnip::Activity)
+    end
+
+    it 'returns json passed if could not parse json' do
+      message = @stream.handle_message('lol')
+      expect(message).to eq('lol')
+    end
+  end
+
+  describe '#parse_json' do
+    before do
+      @json = File.read('spec/fixtures/activities/real_activity.json')
+    end
+
+    it 'does not throw error with activity json' do
+      expect(Proc.new{@stream.parse_json(@json)}).not_to raise_error
+    end
+
+    it 'parses json' do
+      parsed = @stream.parse_json @json
+      expect(parsed.class).to eq(Hash)
+    end
+
+    it 'returns nil if empty json is passed' do
+      parsed = @stream.parse_json('')
+      expect(parsed).to eq(nil)
+    end
+    
+    it 'returns nil if invalid json is passed' do
+      parsed = @stream.parse_json('12,4.,x_VZxuv8{ak{f}}}}} {{ (d(s)aa(((aaaaa)aaa)')
+      expect(parsed).to eq(nil)
+    end
+    
+  end
 end
