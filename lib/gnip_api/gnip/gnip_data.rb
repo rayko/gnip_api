b0b523adfa113b04c382afedec772838de576f1c
Rayko <rayko.drg@gmail.com>
2015-10-15 12:07:55 -0300

    Fixed error in gnip data

--- lib/gnip_api/gnip/gnip_data.rb
+++ lib/gnip_api/gnip/gnip_data.rb
@@ -4,7 +4,7 @@ module Gnip
 
     def initialize params={}
       @matching_rules = params['matching_rules'].map{|r| GnipApi::Apis::PowerTrack::Rule.new(r)} if params['matching_rules']
-      @urls = params['urls'].map{|u| Gnip::Url.new(u)}
+      @urls = (params['urls'] ? params['urls'].map{|u| Gnip::Url.new(u)} : [])
       @language = params['language']
     end
 
