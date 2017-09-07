4e75117994aae090c0be300c954dd158416af41d
Rayko <rayko.drg@gmail.com>
2017-09-07 16:42:41 -0300

    Changing default logger level

--- lib/gnip_api/configuration.rb
+++ lib/gnip_api/configuration.rb
@@ -1,4 +1,12 @@
 module GnipApi
+  # Configurations for the GnipApi gem.
+  #
+  # Defaults:
+  # - logger: *Logger.new('tmp/gnip_api.log')*
+  # - request_timeout: *60*
+  # - debug: *false*
+  # - enable_gzip: *true*
+  # - log_level: *Logger::WARN*
   class Configuration
     attr_accessor :user, :password, :account, :logger, :source, :label, :request_timeout, :debug, 
                   :enable_gzip, :log_level, :buffer_limit
@@ -8,7 +16,7 @@ module GnipApi
       @request_timeout = 60
       @debug = false
       @enable_gzip = true
-      @log_level = Logger::INFO
+      @log_level = Logger::WARN
       @logger.level = @log_level
     end
   end
